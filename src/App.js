4c6ae8918dc98a725a804d837c9a7c12a590945c
Sandra <sandrabalog@rocketmail.com>
2023-04-17 10:53:58 +0200

    add pages from html to react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-
 import './App.css';
+import { Navbar } from './components/Navbar';
+import Dashboard from './pages/Dashboard';
+import Detalji from './pages/Detalji';
+import Home from './pages/Home';
+import Putovanja from './pages/Putovanja';
 
-const App =()=> {
+const App = () => {
   return (
     <div className="App">
-      
+      <Navbar />
+      <Home />
+      <Putovanja />
+      <Detalji />
+      <Dashboard />
     </div>
   );
-}
+};
 
 export default App;
